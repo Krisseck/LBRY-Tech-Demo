f68a79214eccd16b077c88a9cb22812661c72efe
Kristian Polso <kristian@vaiste.com>
2018-02-25 13:52:35 +0200

    Restructure

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app>
+    <v-container grid-list-md text-xs-center>
+      <v-layout row wrap id="navigation">
+        <v-flex xs4>
+          <router-link to="/step-1">
+            <v-btn color="success" fab>1</v-btn>
+            Resolve a claim
+          </router-link>
+        </v-flex>
+        <v-flex xs4>
+          <router-link to="/step-2">
+            <v-btn color="success" fab>2</v-btn>
+            Download a stream
+          </router-link>
+        </v-flex>
+        <v-flex xs4>
+          <router-link to="/step-3">
+            <v-btn color="success" fab>3</v-btn>
+            Support with LBC
+          </router-link>
+        </v-flex>
+      </v-layout>
+      <router-view/>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+
+#navigation {
+  a {
+    text-decoration: none;
+  }
+}
+
+</style>
